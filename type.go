f9f101e17d98e4f7301f03dd55ec6b8b5b97159d
Charles Iliya Krempeaux <iamreiver@gmail.com>
2026-07-10 04:41:03 -0700

    type

--- type.go
+++ type.go
@@ -3,5 +3,6 @@ package tensor
 // Type specifies the parameterized types that are permitted for [Tensor].
 type Type interface {
 	float32 | float64 |
-	int64
+	int8  | int16  | int32  |  int64 |
+	uint8 | uint16 | uint32 | uint64
 }
